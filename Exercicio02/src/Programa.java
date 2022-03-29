d18c3629b8588d00c3fe4c32e749e095d1aa827b
Vinicius Lima <vinicius.damas@hotmail.com>
2022-03-29 19:55:20 -0300

    versão inicial

---
+++ Exercicio02/src/Programa.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Programa {
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in) {
+		String [] matriculas = new String[5];
+		String [] nomes = new String[5];
+		
+		double [] nota1 = new double[5];
+		double [] nota2 = new double[5];
+		
+		double [] notaFinal = new double[5];
+		
+		int i;
+		
+		for(i=0; i<=4; i++) {
+			System.out.println("Digite a matricula " + (i + 1) + ":");
+			matriculas[i] = leitor.nextLine();
+			System.out.println("Digite nome "+ (i + 1) + ":");
+			nomes[i] = leitor.nextLine();
+			System.out.println("Digite a nota 1: ");
+			nota1[i] = leitor.nextDouble();
+			System.out.println("Digite a nota 2: ");
+			nota2[i] = leitor.nextDouble();
+			System.out.println("_____________________\n");
+			leitor.nextLine();
+			notaFinal[i] = (nota1[i] + nota2[i])/2;
+		}
+		
+		for(i=0; i<=4; i++) {
+		System.out.println("Matricula: " + matriculas[i]);
+		System.out.println("Nome: " + nomes[i]);
+		//igual ou superior a 6
+			if (notaFinal[i] >=6) {
+				System.out.println("Aprovado: (x) Sim ( ) Nao");
+				
+			} else {
+				System.out.println("Aprovado: ( ) Sim (x) Nao");
+			}
+		System.out.println("Nota final " + notaFinal[i]);
+		System.out.println("__________________________\n");
+		}
+		
+		leitor.close();
+}
+	}
+
+	
---
+++ Exercicio04/src/Programa.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Programa {
+	
+
+	public static void main(String[] args){
+		
+/*		double[] pesos = new double[5];
+		pesos[0] = 0.05;*/
+
+		double[] pesos = {0.05, 0.05, 0.2, 0.2, 0.5};
+		
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Informe a matrícula:");
+		String matricula = leitor.nextLine();
+		
+		System.out.println("Informe o nome:");
+		String nome = leitor.nextLine();
+		
+		double[] notas = new double[5];
+		
+		int contador = 0;
+		do {
+			System.out.println("Informar uma nota entre 0 e 10.");
+			System.out.println("Digite a " + (contador + 1) + "a nota:");
+			double temp = leitor.nextDouble();
+			
+			if (temp < 0 || temp > 10) {
+				//inválido
+				System.out.println("A nota " + temp + " é inválida!");
+			} else {
+				notas[contador] = temp;
+				contador++;
+			}
+			
+			
+		}while (contador < 5);
+		
+		double notaFinal = 
+				(notas[0] * pesos [0]) +
+				(notas[1] * pesos [1]) +
+				(notas[2] * pesos [2]) +
+				(notas[3] * pesos [3]) +
+				(notas[4] * pesos [4]);
+		
+		System.out.println();
+		System.out.println("-----------RELATÓRIO-----------");
+		System.out.println("Matrícula: " + matricula);
+		System.out.println("Nome: " + nome);
+		
+		
+		
+		for(int i = 0; i < notas.length; i++) {
+			System.out.println("Nota " + (i +1) + ":" + notas[i]);
+		}
+		
+		
+		System.out.println("Nota final: " + notaFinal);
+		
+		if(notaFinal >= 6) {
+			System.out.println("Aprovado!");
+		} else {
+			System.out.println("Reprovado!");
+		}
+				
+		leitor.close();
+}
+}
